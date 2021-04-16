08caf681169f3f7c0732b965dd2cb837cd6b4a40
R M AKSHAY KUMAR <rmakshaykumar08@gmail.com>
2021-04-16 22:37:18 +0530

    added Task Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Hello",
+      day: "feb 5th, 2:30pm"
+    },
+    {
+      id: 2,
+      text: "Bye",
+      day: "Mar 6th, 3:30pm"
+    },
+    {
+      id: 3,
+      text: "Hello, Bye",
+      day: "Apl 7th, 4:30pm"
+    }
+  ]
+  )
+
+  //delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
   return (
     <div className="container">
       <Header title={"Task Tracker"} />
+      {tasks.length > 0? <Tasks tasks={tasks} onDelete={ deleteTask }/>: 'No Tasks'}
     </div>
   );
 }
